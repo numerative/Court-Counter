e0595c26f5d6093b2137fc4fc5e95d7076d0ff4a
numerative <michael.hathi@gmail.com>
2018-03-07 17:43:45 +0530

    First Commit on Cricket Score Keeper mod 	Complete Layout Overhaul

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -6,14 +6,14 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+/*
 
     int scoreTeamA = 0;
     int scoreTeamB = 0;
 
-    /**
+    *//**
      * Displays the given score for Team A.
-     */
+     *//*
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
@@ -35,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    /**
+    *//**
      * Displays the given score for Team B.
-     */
+     *//*
     public void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
@@ -58,20 +58,19 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamB(scoreTeamB);
     }
 
-    /**
+    *//**
      * Luckily, this code will reset all Values.
-     */
+     *//*
     public void resetScore(View v) {
         scoreTeamA = 0;
         scoreTeamB = 0;
         displayForTeamA(scoreTeamA);
         displayForTeamB(scoreTeamB);
-    }
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(0);
     }
 }
